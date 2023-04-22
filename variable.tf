afaaae47e29b14187cdd3f23001b873772d3ef5f
“Akinyemi <micakinyemi@gmail.com>
2023-04-21 23:54:41 -0600

    vpc terraform

--- variable.tf
+++ variable.tf
@@ -14,3 +14,38 @@ variable "project_title" {
   description = "title of project"
   type = string
 }
+
+variable "vpc_cidr" {
+  description = "vpc cidr block"
+  type = string
+}
+
+variable "public_subnet_az1_cidr" {
+  description = "Public Subnet az1 cidr"
+  type = string
+}
+
+variable "public_subnet_az2_cidr" {
+  description = "Public Subnet az2 cidr"
+  type = string
+}
+
+variable "private_app_subnet_az1_cidr" {
+  description = "Public app Subnet az1 cidr block"
+  type = string
+}
+
+variable "private_app_subnet_az2_cidr" {
+  description = "Public app Subnet az2 cidr block"
+  type = string
+}
+
+variable "private_data_subnet_az1_cidr" {
+  description = "Public data Subnet az1 cidr block"
+  type = string
+}
+
+variable "private_data_subnet_az2_cidr" {
+  description = "Public data Subnet az2 cidr block"
+  type = string
+}
